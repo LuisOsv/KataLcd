45ca54709638a44b508145069bfee3d0a7706511
osqsync <osvaldo.serna@qsynccorp.com>
2019-10-16 22:46:39 -0400

    fixing tests

--- src/test/java/TestMultipleDigits.java
+++ src/test/java/TestMultipleDigits.java
@@ -4,24 +4,6 @@ import org.junit.Test;
 public class TestMultipleDigits {
 
     @Test
-    public void testTwoDigits(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("\t \t _\n");
-        sb.append("\t|\t _|\n");
-        sb.append("\t|\t|_\n");
-        Assert.assertEquals(sb.toString(),NumberToLCD.convertMultipleDigits(12));
-    }
-
-    @Test
-    public void testThreeDigits(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("\t \t _\t _\n");
-        sb.append("\t|\t _|\t _|\n");
-        sb.append("\t|\t|_\t _|\n");
-        Assert.assertEquals(sb.toString(),NumberToLCD.convertMultipleDigits(123));
-    }
-
-    @Test
     public void testAllDigits(){
         StringBuilder sb = new StringBuilder();
         sb.append("\t \t _\t _\t   \t _ \t _ \t _ \t _ \t _ \n");
